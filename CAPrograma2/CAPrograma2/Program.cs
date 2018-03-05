202ec19f555e53c99623aaef35a9cbe5e5b71528
Leandro Tk <leandrotk100@gmail.com>
2018-03-04 21:12:13 -0300

    Added program 2 and 3

---
+++ CAPrograma2/CAPrograma2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPrograma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int x, y, z;
+
+            Console.WriteLine("Digite X:");
+            s = Console.ReadLine();
+            x = int.Parse(s);
+
+            Console.WriteLine("Digite Y:");
+            s = Console.ReadLine();
+            y = int.Parse(s);
+
+            Console.WriteLine("Adição: " + (x + y));
+            Console.WriteLine("Subtração: " + (x - y));
+            Console.WriteLine("Multiplicação: " + (x * y));
+            Console.WriteLine("Divisão: " + (x / y));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CAPrograma3/CAPrograma3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPrograma3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, input;
+            int age, readjust;
+            double salary;
+
+            Console.Write("Nome: ");
+            name = Console.ReadLine();
+            Console.Write("Idade: ");
+            input = Console.ReadLine();
+            age = int.Parse(input);
+            Console.Write("Salário: ");
+            input = Console.ReadLine();
+            salary = double.Parse(input);
+
+            Console.WriteLine("Reajuste (em %)");
+            input = Console.ReadLine();
+            readjust = int.Parse(input);
+
+            Console.WriteLine("Dados lidos");
+            Console.WriteLine("==========");
+
+            Console.WriteLine("Nome: " + name);
+            Console.WriteLine("Idade: " + age);
+            Console.WriteLine("Salário: " + salary);
+            Console.WriteLine("Salário com reajuste: " + (salary + salary * readjust / 100));
+            Console.ReadKey();
+        }
+    }
+}
