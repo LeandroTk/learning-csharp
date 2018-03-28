71c1d1fb03f96d96a9080e28c35d70a25c04ec02
Leandro Tk <leandrotk100@gmail.com>
2018-03-28 19:49:24 -0300

    Program 4

---
+++ CAPrograma4/CAPrograma4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPrograma4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite capacidade do elevador: ");
+            string s = Console.ReadLine();
+            double capacidade = double.Parse(s);
+
+            Console.WriteLine("Digite capacidade a ser transportada: ");
+            s = Console.ReadLine();
+            double carga = double.Parse(s);
+
+            double viagens = carga / capacidade;
+            int numeroDeViagens = (int)(viagens);
+
+            if (viagens > numeroDeViagens)
+            {
+                numeroDeViagens++;
+            }
+
+            Console.WriteLine("Viagens necessárias: " + numeroDeViagens.ToString());
+            Console.ReadKey();
+        }
+    }
+}
